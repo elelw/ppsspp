066433aa7a833f67afb45a8039b09efcf26fa494
Angelo <angelo@Angelo-HP>
2016-10-07 23:40:50 +0000

    Fix bug for axis

--- ios/ViewController.mm
+++ ios/ViewController.mm
@@ -475,12 +475,12 @@ int ToTouchID(UITouch *uiTouch, bool allowAllocate) {
 			switch (button) {
 				case iCadeLeftAxisUp :
 					axis.axisId = JOYSTICK_AXIS_Y;
-					axis.value = 1.0f;
+					axis.value = -1.0f;
 					break;
 					
 				case iCadeLeftAxisDown :
 					axis.axisId = JOYSTICK_AXIS_Y;
-					axis.value = -1.0f;
+					axis.value = 1.0f;
 					break;
 					
 				case iCadeLeftAxisLeft :
@@ -507,12 +507,12 @@ int ToTouchID(UITouch *uiTouch, bool allowAllocate) {
         switch (button) {
             case iCadeRightAxisUp :
                 axis.axisId = JOYSTICK_AXIS_Z;
-                axis.value = 1.0f;
+                axis.value = -1.0f;
                 break;
                 
             case iCadeRightAxisDown :
                 axis.axisId = JOYSTICK_AXIS_Z;
-                axis.value = -1.0f;
+                axis.value = 1.0f;
                 break;
                 
             case iCadeRightAxisLeft :
